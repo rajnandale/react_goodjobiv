ae743286a3360f93c27363363cb213c4ba3e7a36
Raj Nandale <rajnandale6164@gmail.com>
2024-09-21 02:40:09 +0530

    19th commit

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import { Route } from 'react-router-dom';
-import JobRoles from './pages/JobRoles';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
+import JobRoles from '../pages/JobRoles';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Home from '../pages/Home';
 
 const routes = [
   { path: '/', element: <Home /> },
